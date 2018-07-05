9d92fed203f87177c6a517cebf57f5c4c4dd8954
hanannadir <40139951+hanannadir@users.noreply.github.com>
2018-07-05 15:51:46 +0300

    child class in program

---
+++ Cats.java
@@ -0,0 +1,18 @@
+package com.example.android.javaclass;
+
+import android.util.Log;
+
+/**
+ * Created by IT LOBBY on 7/5/2018.
+ */
+
+public class Cats extends Animals {
+
+    @Override
+    public void justPrintSomething(){
+        Log.d("Cats Class", "this is method in Cats class  ");
+
+    }
+
+
+}
